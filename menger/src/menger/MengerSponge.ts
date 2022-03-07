ed64278d1c23faeffab31da47d2d5ebb97f6b339
Allen Zheng <allenxzheng@gmail.com>
2022-03-07 04:11:41 -0600

    menger recursive working

--- menger/src/menger/MengerSponge.ts
+++ menger/src/menger/MengerSponge.ts
@@ -1,4 +1,8 @@
 import { Mat3, Mat4, Vec3, Vec4 } from "../lib/TSM.js";
+import { normHelper, faceHelper, cubeVertices, flattenListOfVec } from "./Utils.js";
+
+var debug = false;
+var debug_cubes = 0;
 
 /* A potential interface that students should implement */
 interface IMengerSponge {
@@ -16,10 +20,53 @@ interface IMengerSponge {
 export class MengerSponge implements IMengerSponge {
 
   // TODO: sponge data structures
-  
-  constructor(level: number) {
-	  this.setLevel(level);
-	  // TODO: other initialization	
+  level:number;
+
+  vertices:Vec4[];
+  faces:Vec3[];
+  normals:Vec4[];
+
+  positions_array:Float32Array;
+  face_array:Uint32Array;
+  normals_array:Float32Array;
+
+  private recursiveBuild(curr_level: number, xmin: number, ymin: number, zmin: number, len: number): void {
+    if(curr_level == 1)
+    {
+      if(debug)
+        console.log("Drawing Cube #" + ++debug_cubes + " at " + xmin + " " + ymin + " " + zmin + " " + "len: " + len + "\n");
+      this.faces.push(...faceHelper(this.vertices.length));
+      this.vertices.push(...cubeVertices(xmin, ymin, zmin, len));
+      this.normals.push(...normHelper());
+      return;
+    }
+    var x = xmin;
+    var y = ymin;
+    var z = zmin;
+    var newLen = len/3.0;
+    for(var cube = 0; cube < 27; cube++)
+    {
+      //middle cubes
+      if(cube == 4 || cube == 10 || cube == 12 || cube == 13 || cube == 14 || cube == 16 || cube == 22)
+        continue;
+      x = xmin + (cube % 3)*newLen;
+      y = ymin + (Math.floor(cube/3) % 3)*newLen;
+      z = zmin + (Math.floor(cube/9))*newLen;
+      this.recursiveBuild(curr_level - 1, x, y, z, newLen);
+    }
+  }
+
+  constructor(level: number, ) {
+    this.vertices = [];
+    this.faces = [];
+    this.normals = [];
+    this.setLevel(level);
+	  // TODO: other initialization
+
+
+    this.positions_array = new Float32Array(flattenListOfVec(this.vertices));
+    this.face_array = new Uint32Array(flattenListOfVec(this.faces));
+    this.normals_array = new Float32Array(flattenListOfVec(this.normals));
   }
 
   /**
@@ -35,12 +82,16 @@ export class MengerSponge implements IMengerSponge {
   public setLevel(level: number)
   {
 	  // TODO: initialize the cube
+    this.level = level;
+    this.recursiveBuild(level, -.5, -.5, -.5, 1);
   }
+
+
 
   /* Returns a flat Float32Array of the sponge's vertex positions */
   public positionsFlat(): Float32Array {
 	  // TODO: right now this makes a single triangle. Make the cube fractal instead.
-	  return new Float32Array([1.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0]);
+    return this.positions_array;
   }
 
   /**
@@ -48,7 +99,7 @@ export class MengerSponge implements IMengerSponge {
    */
   public indicesFlat(): Uint32Array {
     // TODO: right now this makes a single triangle. Make the cube fractal instead.
-    return new Uint32Array([0, 1, 2]);
+    return this.face_array;
   }
 
   /**
@@ -56,7 +107,8 @@ export class MengerSponge implements IMengerSponge {
    */
   public normalsFlat(): Float32Array {
 	  // TODO: right now this makes a single triangle. Make the cube fractal instead.
-	  return new Float32Array([0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]);
+
+	  return this.normals_array;
   }
 
   /**
@@ -68,6 +120,6 @@ export class MengerSponge implements IMengerSponge {
     const ret : Mat4 = new Mat4().setIdentity();
 
     return ret;    
-  }
+  } 
   
 }
--- menger/src/menger/Utils.ts
+++ menger/src/menger/Utils.ts
@@ -4,6 +4,8 @@ import { Color } from "../lib/Colors.js";
 import { Vec2, Vec3, Vec4 } from "../lib/TSM.js";
 import _ from "../lib/Underscore.js";
 
+var debug = false;
+
 /*
     List Utility Functions
 */
@@ -29,3 +31,102 @@ export function flattenListOfVec(listofVec: Vec3[] | Vec4[]): number[] {
 
 // You will likely want to add extra helper functions throughout your project.
 // Feel free to do so here!
+
+//8 vertices of a cube from dimensions. Default (-.5, -.5, -.5) w/ len 1.0 -> list of triangle vertices
+export function cubeVertices(xmin: number, ymin: number, zmin: number, len: number): Vec4[] {
+      // (x1, y1, z1), (x2, y2, z2) -> 8 vertices
+    var x = xmin;
+    var y = ymin;
+    var z = zmin;
+
+    let vertices:Vec4[] = [];
+    for(var index = 0; index < 8; index++)
+    {
+        x = (index % 2 < 1) ? xmin: xmin + len;
+        y = (index % 4 < 2) ? ymin: ymin + len;
+        z = (index % 8 < 4) ? zmin: zmin + len;
+        var vec = new Vec4([x, y, z, 1.0]); //points, w = 1
+        vertices.push(vec);
+        if(debug)
+          console.log("vec" + index + ": " + x + " " + y + " " + z + " " + "\n");
+    }
+  
+  //from these 8, construct cube in this order s.t. face indices can be [0, 1, 2], [3, 4, 5]
+
+  let ans:Vec4[] = [];
+  //norm +x left face 5173
+  ans.push(vertices[7]);
+  ans.push(vertices[1]);
+  ans.push(vertices[5]);
+  ans.push(vertices[1]);
+  ans.push(vertices[7]);
+  ans.push(vertices[3]);
+  //norm +y top face 6723
+  ans.push(vertices[2]);
+  ans.push(vertices[7]);
+  ans.push(vertices[6]);
+  ans.push(vertices[7]);
+  ans.push(vertices[2]);
+  ans.push(vertices[3]);
+  //norm: +z front face 4567
+  ans.push(vertices[6]);
+  ans.push(vertices[5]);
+  ans.push(vertices[4]);
+  ans.push(vertices[5]);
+  ans.push(vertices[6]);
+  ans.push(vertices[7]);
+  //norm -x right face 0426
+  ans.push(vertices[2]);
+  ans.push(vertices[4]);
+  ans.push(vertices[0]);
+  ans.push(vertices[4]);
+  ans.push(vertices[2]);
+  ans.push(vertices[6]);
+  //norm: -y bottom face 0145
+  ans.push(vertices[4]);
+  ans.push(vertices[1]);
+  ans.push(vertices[0]);
+  ans.push(vertices[1]);
+  ans.push(vertices[4]);
+  ans.push(vertices[5]);
+  //norm -z back face 1032
+  ans.push(vertices[3]);
+  ans.push(vertices[0]);
+  ans.push(vertices[1]);
+  ans.push(vertices[0]);
+  ans.push(vertices[3]);
+  ans.push(vertices[2]);
+  return ans;
+}
+
+export function faceHelper(offset: number): Vec3[] {
+  let ans:Vec3[] = [];
+  // above verts are ordered in such a way that I can do 0, 1, 2, 3, 4, 5
+  for(var face = 0; face < 6; face++)
+  {
+    var v1 = new Vec3([offset, offset + 1, offset + 2]);
+    var v2 = new Vec3([offset + 3, offset + 4, offset + 5]);
+    offset += 6;
+    ans.push(v1);
+    ans.push(v2);
+  }
+  return ans;
+}
+
+export function normHelper(): Vec4[] {
+  let ans:Vec4[] = [];
+  var x = 0;
+  var y = 0;
+  var z = 0;
+  for(var face = 0; face < 6; face++)
+  {
+    x = (face % 3 == 0) ? ((face == 0) ? 1.0: -1.0) : 0.0;
+    y = (face % 3 == 1) ? ((face == 1) ? 1.0: -1.0) : 0.0;
+    z = (face % 3 == 2) ? ((face == 2) ? 1.0: -1.0) : 0.0;
+    var vec = new Vec4([x, y, z, 0.0]); //vectors, w = 0.0
+    //one vector per vertex. six vectors per face
+    for(var i = 0; i < 6; i++)
+      ans.push(vec);
+  }
+  return ans;
+}
