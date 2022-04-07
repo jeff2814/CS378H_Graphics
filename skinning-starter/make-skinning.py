f0f7afd30f571592b83a5b0fc37c92e9e153aeaf
Jeffery Wu <jeff2814@hotmail.com>
2022-04-07 08:52:01 -0500

    Why not add the make-skinning

--- skinning-starter/make-skinning.py
+++ skinning-starter/make-skinning.py
@@ -5,7 +5,7 @@ import shutil
 
 srcfiles = glob.glob('./src/skinning/*.ts')
 loaders = glob.glob('./src/lib/threejs/examples/jsm/loaders/*.js')
-cmd = 'tsc --allowJs -m ES6 -t ES6 --outDir dist --sourceMap --alwaysStrict ' + " ".join(srcfiles) + ' ./src/lib/vue/vue.js ' + " ".join(loaders)
+cmd = 'tsc --allowJs -m ES6 -t ES6 --outDir dist --sourceMap --alwaysStrict --incremental --tsBuildInfoFile .tsbuildinfo ' + " ".join(srcfiles) + ' ./src/lib/vue/vue.js ' + " ".join(loaders)
 print('Building TypeScript: ' + cmd)
 os.system(cmd)
 copy_tree('./src/skinning/static', './dist')
